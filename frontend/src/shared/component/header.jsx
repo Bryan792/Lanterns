ebcbce02e5b60534cc4c2271c79d1b8bbdf96ace
Bryan Ching <bryan792@gmail.com>
2017-04-01 04:05:00 -0700

    Center the header

--- frontend/src/shared/component/header.jsx
+++ frontend/src/shared/component/header.jsx
@@ -7,7 +7,7 @@ import { APP_NAME } from '../config'
 import { HOME_PAGE_ROUTE } from '../routes'
 
 const Header = () =>
-  <header>
+  <header style={{ textAlign: 'center' }}>
     <NavLink to={HOME_PAGE_ROUTE} style={{ color: 'black', textDecoration: 'none' }}>
       <h1>{APP_NAME}</h1>
     </NavLink>
